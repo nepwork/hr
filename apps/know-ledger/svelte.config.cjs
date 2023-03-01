393e6990f8b77e83b36f2aa69a3c7f16156c05cf
Risav Karna <git@risav.dev>
2023-03-02 00:51:07 +0545

    build(kl): setup svelte tailwind

--- apps/know-ledger/svelte.config.cjs
+++ apps/know-ledger/svelte.config.cjs
@@ -3,5 +3,8 @@ const sveltePreprocess = require('svelte-preprocess');
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: sveltePreprocess({
+    // WARNING: Mandatory for the postcss config (postcss.config.js) and Tailwind to be loaded
+    postcss: {}, // <------ this is the important part :)
+  }),
 };
